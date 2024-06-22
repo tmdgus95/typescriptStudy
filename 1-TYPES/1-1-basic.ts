e98328fdec411e6768af79099cd8e61a175d5ec6
tmdgus <8571053@naver.com>
2024-06-23 00:39:50 +0900

    기본 타입 정리

---
+++ 1-TYPES/1-1-basic.ts
@@ -0,0 +1,27 @@
+{
+  /**
+   * JavaScript
+   * Primitive: number, string, boolean, bigint, symbol, null, undifined
+   * Object: funtion, array.....
+   */
+
+  //number
+  const num: number = -6;
+
+  //string
+  const str: string = "hello";
+
+  //boolean
+  const boal: boolean = false;
+
+  //undifined
+  let name: string | undefined;
+  name = undefined;
+  name = "tmdgus";
+  function find(): number | undefined {
+    return undefined;
+  }
+
+  //null
+  let person: string | null;
+}
