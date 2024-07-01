7f9d2faf01f55563d6b597483dcc75587ea34720
tmdgus <8571053@naver.com>
2024-07-02 00:49:07 +0900

    Enum

---
+++ 1-TYPES/1-8-enum.ts
@@ -0,0 +1,34 @@
+{
+  /**
+   * Enum
+   */
+  // JavaScript
+  const MAX_NUM = 6;
+  const MAX_STUDENTS_PER_CLASS = 10;
+  const MONDAY = 0;
+  const TUESDAY = 1;
+  const WEDNESDAY = 2;
+  const DAYS_ENUM = Object.freeze({ MONDAY: 0, TUESDAY: 1, WEDNESDAY: 2 });
+  const dayOfToday = DAYS_ENUM.MONDAY;
+
+  // TypeScript
+  type DaysOfWeek = "Monday" | "Tuesday" | "Wednesday";
+  enum Days {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+  console.log(Days.Monday);
+  let day: Days = Days.Saturday;
+  day = Days.Tuesday;
+  // 최신버전에서는 10할당 불가능
+  //day = 10;
+  console.log(day);
+
+  let dayOfweek: DaysOfWeek = "Monday";
+  dayOfweek = "Wednesday";
+}
