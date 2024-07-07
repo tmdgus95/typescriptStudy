d26972dafef30ee724d1cb215813af0a57543bc6
tmdgus <8571053@naver.com>
2024-07-07 23:21:35 +0900

    로딩 상태표시

---
+++ 2-TYPES-QUIZ/loading.ts
@@ -0,0 +1,36 @@
+{
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: "loading";
+  };
+
+  type SuccessState = {
+    state: "success";
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: "fail";
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+
+  function printLoginState(state: ResourceLoadState) {
+    if (state.state === "loading") {
+      console.log(state.state);
+    } else if (state.state === "success") {
+      console.log(state.response.body);
+    } else if (state.state === "fail") {
+      console.log(state.reason);
+    }
+  }
+}
